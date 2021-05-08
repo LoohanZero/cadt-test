0e49e521ac87b79c3753763f6a1f03111525d4d2
lujansanchez <lsanchez@visual-engin.com>
2021-05-08 12:49:43 +0200

    added sass package and linted all scss files

--- src/components/aside/Aside.test.js
+++ src/components/aside/Aside.test.js
@@ -4,12 +4,16 @@ import '@testing-library/jest-dom/extend-expect';
 
 import { render } from '@testing-library/react';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Aside from './Aside.js';
 
 
 test('renders content', () => {
-	const component = render(<Aside/>);
+	const component = render(
+		<Router>
+			<Aside />
+		</Router>);
 
 	component.getByText('Designs');
 	component.getByText('Setouts');
