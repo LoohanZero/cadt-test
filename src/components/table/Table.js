f626d2592ef845e6967e595aeaaaaa7d5ac99d4f
lujansanchez <lsanchez@visual-engin.com>
2021-05-08 22:17:41 +0200

    imported styles in Table component and added style for heading

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,23 +1,16 @@
-/* eslint-disable no-console */
+import './table.scss';
+
 import React from 'react';
 
-// const reduceTitles = (accumulatedTitles, newItem) => {
-// 	for (const key in newItem) {
-// 		if(!accumulatedTitles.includes(key)) {
-// 			return [ ...accumulatedTitles, key ];
-// 		}
-// 	}
-// 	return accumulatedTitles;
-// };
+
 
 const Table = ({ data, titles }) => {
-	// const titles = data.reduce(reduceTitles, []);
-	console.log(data);
+
 
 	return (
 		<div>
 			<ul>
-				<li className="table-heading-list-item">
+				<li className='table-heading-list-item'>
 					{titles?.map((title, index) => 
 						(<div key={index}>
 							<h2>{title}</h2>
