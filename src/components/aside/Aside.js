d31f11060dfa74f541fa0d750c66e0b060793313
lujansanchez <lsanchez@visual-engin.com>
2021-05-08 14:39:44 +0200

    added scss to package.json, finished styles of aside

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -1,13 +1,19 @@
 import './aside.scss';
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Aside = () => {
 	return (
 		<aside className='aside-container'>
-			<Link to='/designs'>Designs</Link>
-			<Link to='/setouts'>Setouts</Link>
+			<nav className='aside-nav'>
+				<NavLink className='aside-nav-link' activeClassName="selected" to='/designs' >
+					<p className='aside-nav-link-text'>Designs</p>
+				</NavLink>
+				<NavLink className='aside-nav-link' activeClassName="selected" to='/setouts'>
+					<p className='aside-nav-link-text'>Setouts</p>
+				</NavLink>
+			</nav>
 		</aside>
 	);
 };
