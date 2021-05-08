c1f92a7a359cae01e505f5beac845d0fb6b1f899
lujansanchez <lsanchez@visual-engin.com>
2021-05-08 22:58:39 +0200

    added styling to table and added some global variables for font size

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -6,17 +6,32 @@ import React from 'react';
 
 const Table = ({ data, titles }) => {
 
-
 	return (
-		<div>
-			<ul>
-				<li className='table-heading-list-item'>
+		<div className='table-list-container'>
+			<ul className='table-list'>
+				<li className='table-row-styling'>
 					{titles?.map((title, index) => 
-						(<div key={index}>
-							<h2>{title}</h2>
+						(<div className='table-row-item-container' key={index}>
+							<h2 className='table-column-title'>{title}</h2>
 						</div>))}
 				</li>
-				{data?.map((item, index) => <li key={index}>{item.id}</li>)}
+				{data?.map(item => 
+					<li key={item.id} >
+						<a className='table-row-styling'>
+							<div className='table-row-item-container'>
+								<p>{item.name}</p>
+							</div>
+							<div className='table-row-item-container'>
+								<p>{item.courses}</p>
+							</div>
+							<div className='table-row-item-container'>
+								<p>{item.wales}</p>
+							</div>
+							<div className='table-row-item-container'>
+								<p>{item.name}</p>
+							</div>
+						</a>
+					</li>)}
 			</ul>
             
 		</div>
