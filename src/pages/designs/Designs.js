c4eb6b7b111aefa69b6af6a1b2c30f016761acbe
lujansanchez <lsanchez@visual-engin.com>
2021-05-08 16:50:30 +0200

    added tests for page designs and added spinner for loading

--- src/pages/designs/Designs.js
+++ src/pages/designs/Designs.js
@@ -1,10 +1,58 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useReducer } from 'react';
+import GridLoader from 'react-spinners/GridLoader';
+
+import { getDesignData } from '../../services/getData';
+
+const designsModel = { isLoading: false, 
+	data: null, 
+	error: null };
+
+const reducerDesigns = (state, action) => {
+	switch (action.type) {
+		case 'isLoading':
+			return { ...state, isLoading: !state.isLoading };
+		case 'data':
+			return { ...state, data: action.payload };
+		default:
+			return { ...state, error: new Error() };
+	}
+};
+
 
 const Designs = () => {
+	const [ designs, dispatchDesings ] = useReducer(reducerDesigns, designsModel);
+
+
+	const getDesigns = async () => {
+		dispatchDesings({ type: 'isLoading' });
+
+		const designsInformation = await getDesignData();
+		
+		dispatchDesings({ type: 'data', payload: designsInformation });
+		dispatchDesings({ type: 'isLoading' });
+		
+	};
+
+	useEffect(() => {
+		getDesigns();
+
+		return () => {
+			dispatchDesings({ type: 'designs', payload: null });
+		};
+	}, [ ]);
+	// eslint-disable-next-line no-console
+	console.log(designs.isLoading, designs.data);
 	return (
-		<div>
+		<main clasName=''>
+			{designs.isLoading && 
+			<div>
+				<GridLoader/>
+			</div>
+			}
+			{!designs.isLoading && designs.data && <li></li>}
             
-		</div>
+		</main>
 	);
 };
 
---
+++ src/pages/designs/Designs.test.js
@@ -0,0 +1,30 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-undef */
+import '@testing-library/jest-dom/extend-expect';
+
+import { render, waitFor } from '@testing-library/react';
+import React from 'react';
+
+import Designs from './Designs.js';
+
+beforeAll(() => jest.spyOn(window, 'fetch'));
+
+test('renders content', () => {
+	const component = render(<Designs/>);
+
+	component.container.querySelector('ul');
+});
+
+
+
+test('loads items eventually', async () => {
+	render(<Designs/>);
+  
+	const mockAPI = window.fetch.mockResolvedValueOnce({
+		ok: true,
+		json: async () => ({ success: true }),
+	});
+	
+	await waitFor(() => expect(mockAPI).toHaveBeenCalledTimes(1));
+	await waitFor(() => expect(mockAPI).toHaveBeenCalledWith('/designs'));
+});
